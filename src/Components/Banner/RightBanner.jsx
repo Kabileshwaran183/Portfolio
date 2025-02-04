b7bd43fc77cc827e02fbbd38b37bd2529a33aaa4
Kabileshwaran183 <Kabileshwaran183@gmail.com>
2025-02-04 16:03:36 +0530

    profile image added

--- src/Components/Banner/RightBanner.jsx
+++ src/Components/Banner/RightBanner.jsx
@@ -1,10 +1,16 @@
+
+
+
 const RightBanner = () => {
     return (
+        <div className="w-full lgl:w-1/2 flex justify-center items-center relative  lg:mt-[-230px]">
+            <img
+                className="w-[200px]  lgl:w-[300px]  z-10"
+                src="/kabi3.png"
+                alt="bannerImg"
+            /><div className="absolute bottom-0 w-[100px] h-[200px] lgl:w-[300px] lgl:h-[300px] bg-gradient-to-r from-[#202327] to-[#646942] shadow-shadowOne flex justify-start items-center"></div>
 
-        <>
-        RightBanner
-        </>
-
+        </div>
     );
 }
 
