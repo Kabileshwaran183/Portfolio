359e1dbb1cd7f57737fd91085fd53b821f5b9f33
Kabileshwaran183 <Kabileshwaran183@gmail.com>
2025-02-05 18:58:02 +0530

    project section

--- src/Components/Banner/Banner.jsx
+++ src/Components/Banner/Banner.jsx
@@ -7,7 +7,7 @@ const Banner = () => {
 
         <section
             id="home"
-            className="w-full xl:pt-10 max-md:pt-5 mdl:pt-8 pb-20 flex flex-col gap-10 max-md:gap-7 xl:gap-0 lgl:flex-row items-center border-b-[1px] font-titleFont border-b-black"
+            className="w-full xl:pt-10 max-md:pt-5 mdl:pt-8 pb-10 flex flex-col gap-10 max-md:gap-7 xl:gap-0 lgl:flex-row items-center border-b-[1px] font-titleFont border-b-black"
         >
             <Left />
             <RightBanner />
--- src/Components/Features.jsx
+++ src/Components/Features.jsx
@@ -1,8 +1,43 @@
+import { SiCanva, SiFigma, SiJavascript, SiReact } from "react-icons/si";
+import Card from "./Card";
+import Title from "./Title";
+import { FaFigma, FaPython } from "react-icons/fa";
+import { DiPhotoshop } from "react-icons/di";
+
 const Features = () => {
     return (
-        <>
-            <h1>Features</h1>
-        </>
+        <section
+            id="features"
+            className="w-full py-10 border-b-[1px] border-b-black"
+        >
+            <Title title="Projects" des="What I Do" />
+            <div className="grid grid-cols-1 md:grid-cols-2 xl:grid-cols-3 gap-6 xl:gap-20">
+                <Card title="React Projects"
+                    des="Lorem ipsum dolor sit amet consectetur adipisicing elit. Atque solutahic consequuntur eum repellendus ad."
+                    icon={<SiReact/>}
+                />
+                <Card title="JavaScript Projects"
+                    des="Lorem ipsum dolor sit amet consectetur adipisicing elit. Atque solutahic consequuntur eum repellendus ad."
+                    icon={<SiJavascript/>}
+                />
+                <Card title="Python Projects"
+                    des="Lorem ipsum dolor sit amet consectetur adipisicing elit. Atque solutahic consequuntur eum repellendus ad."
+                    icon={<FaPython/>}
+                />
+                <Card title="Photoshop Designs"
+                    des="Lorem ipsum dolor sit amet consectetur adipisicing elit. Atque solutahic consequuntur eum repellendus ad."
+                    icon={<DiPhotoshop/>}
+                />
+                <Card title="Figma Designs"
+                    des="Lorem ipsum dolor sit amet consectetur adipisicing elit. Atque solutahic consequuntur eum repellendus ad."
+                    icon={<FaFigma/>}
+                />
+                <Card title="Canva Designs"
+                    des="Lorem ipsum dolor sit amet consectetur adipisicing elit. Atque solutahic consequuntur eum repellendus ad."
+                    icon={<SiCanva/>}
+                />
+            </div>
+        </section>
     );
 }
 
