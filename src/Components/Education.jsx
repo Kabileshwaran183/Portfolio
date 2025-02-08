bc8ce124f285915993f17584a58d11cd98b5c0cd
Kabileshwaran183 <Kabileshwaran183@gmail.com>
2025-02-08 21:24:17 +0530

    responsive education section

--- src/Components/Education.jsx
+++ src/Components/Education.jsx
@@ -4,28 +4,28 @@ const Education = () => {
     return (
         <section
             id="projects"
-            className="w-full pb-52 border-b-[1px] border-b-black"
+            className="w-full pb-24 xl:pb-44 border-b-[1px] border-b-black"
         >
-        <div className="">
-            <div className="py-6 lgl:py-12 font-titleFont flex flex-col gap-4">
+        <div className="max-md:px-4 md:px-7 xl:px-2">
+            <div className=" py-5 md:py-9 font-titleFont flex flex-col gap-2 ">
                 <p className="text-sm text-designColor tracking-[4px]">2019-2021</p>
                 <h2 className="text-3xl md:text-4xl font-bold">Education</h2>
             </div>
-            <div className="mt-2 lgl:mt-8 w-full h-[100px] border-l-[6px] border-l-black border-opacity-30 flex gap-2">
+            <div className="mt-2 lgl:mt-8 w-full max-md:h-[310px] h-[100px] border-l-[6px] border-l-black border-opacity-30 flex max-md:flex-col max-md:gap-8 ">
                 <EducationCard
                     title="BE-Computer Science and Engineering"
                     des="Mount Zion College of Engineering and Technology"
-                    subTitle="2021-25"
+                    subTitle=" (2021-25)"
                     result="7.64" />
                 <EducationCard
                     title="HSC"
                     des="Sri Kalaivani Vidyalaya Matriculation Higher Secondary School"
-                    subTitle="2019-21"
+                    subTitle="(2019-21)"
                     result="90.52%" />
                 <EducationCard
                     title="SSLC"
                     des="Sri Kalaivani Vidyalaya Matriculation Higher Secondary School"
-                    subTitle="2017-19"
+                    subTitle="(2017-19)"
                     result="90.2%" />
             </div>
         </div>
--- src/Components/EducationCard.jsx
+++ src/Components/EducationCard.jsx
@@ -2,37 +2,33 @@ import React from "react";
 
 const EducationCard = ({ title, subTitle, result, des }) => {
     return (
-        <div className="w-full h-[200px] group flex">
-            <div className="w-10 h-[6px] bgOpacity mt-16 relative">
-                <span className="absolute w-5 h-5 -top-[0.49rem] -left-2 flex justify-center items-center">
-                    {result!=7.64 ? <span className="absolute w-0 h-0 border-l-[9px] border-l-transparent border-r-[9px] border-r-transparent border-t-[18px] border-t-[#131312f0]  group-hover:border-t-[design-color] transform -rotate-[-90deg] group-hover:border-t-designColor duration-300"></span>
-                    :          <span className="absolute w-5 h-5 rounded-full -top-16 -left-1 flex justify-center items-center bg-black bg-opacity-60">
-                    <span className="w-3 h-3 rounded-full bg-bodyColor inline-flex group-hover:bg-designColor duration-300"></span>
-                </span>}
+        <div className="w-full md:h-[150px] xl:h-[200px] group flex">
+            <div className="w-10 max-md:w-7 h-[4px] bgOpacity mt-12 max-md:mt-6 relative ">
+                <span className="absolute w-5 h-5 -top-[0.5rem] -left-2 flex justify-center items-center">
+                <span className="absolute w-0 h-0 max-md:border-l-[5px] border-l-[9px] border-l-transparent border-r-[9px] max-md:border-r-[5px] border-r-transparent border-t-[18px] max-md:border-t-[10px] border-t-[#131312f0] group-hover:border-t-designColor transform -rotate-[-90deg] duration-300"></span>
                 </span>
             </div>
-            <div className="w-full bg-black bg-opacity-20 hover:bg-opacity-30 duration-300 rounded-lg p-4 lgl:px-10 flex flex-col justify-center gap-6 lgl:gap-6 shadow-shadowOne">
-                <div className="flex flex-col lgl:flex-row justify-between gap-3 lgl:gap-0 lgl:items-center">
-                    <div>
-                        <h3 className="text-xl md:text-2xl font-semibold group-hover:text-white duration-300">
+            <div className="w-full max-md:px-8 bg-black bg-opacity-20 hover:bg-opacity-30 duration-300 rounded-lg md:px-4 lgl:px-10 xl:py-7 md:py-1 py-2 flex flex-col justify-center max-md:items-start md:gap-5 lgl:gap-6 shadow-shadowOne">
+                <div className="flex flex-row justify-between w-full ">
+                    <div className="flex flex-col">
+                        <h3 className="text-xl md:text-[14px] xl:text-xl max-md:text-[12px] z-10 font-semibold group-hover:text-white duration-300 leading-tight">
                             {title}
                         </h3>
-                        <p className="text-sm mt-2 text-gray-400 group-hover:text-white duration-300">
+                        <p className="text-sm md:text-[11px] xl:text-sm max-md:text-[9px] mt-2 text-gray-400 group-hover:text-white duration-300">
                             {subTitle}
                         </p>
                     </div>
                     <div>
-                        <p className="px-4 py-2 text-designColor group-hover:text-green-400 bg-black bg-opacity-25 rounded-lg flex justify-center items-center shadow-shadowOne text-sm font-medium">
+                        <p className="md:px-2 md:py-1 max-md:px-1 xl:px-3 xl:py-2 text-sm md:text-[10px] xl:text-sm max-md:text-[8px] text-designColor group-hover:text-green-400 bg-black bg-opacity-25 max-md:rounded-sm rounded-lg flex justify-center items-center shadow-shadowOne font-medium">
                             {result}
                         </p>
                     </div>
                 </div>
                 <div>
-                <p className="text-sm md:text-base font-medium text-gray-400 group-hover:text-gray-300 duration-300">
-                    {des}
-                </p>
+                    <p className="text-sm md:text-[11px] max-md:text-[9px] xl:text-sm text-gray-400 group-hover:text-gray-300 duration-300 leading-tight">
+                        {des}
+                    </p>
                 </div>
-
             </div>
         </div>
     );
