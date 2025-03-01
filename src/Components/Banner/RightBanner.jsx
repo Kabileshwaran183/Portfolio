09b32879f2054fddddfc1ffe4edf78951dc2c713
Kabileshwaran183 <Kabileshwaran183@gmail.com>
2025-03-01 19:00:05 +0530

    main img adjusted"

--- src/Components/Banner/RightBanner.jsx
+++ src/Components/Banner/RightBanner.jsx
@@ -5,10 +5,10 @@ const RightBanner = () => {
     return (
         <div className="w-full lgl:w-1/2 flex justify-center items-center relative xl:mt-[-110px] lg:mt-[-160px]">
             <img
-                className="w-[300px]  lg:w-[600px] xl:w-[700px] lg:mt-24    z-10"
-                src="/kabilesh.png"
+                className="w-[300px]  lg:w-[600px] xl:w-[400px] lg:mt-18   z-10"
+                src="/kabinew.png"
                 alt="bannerImg"
-            /><div className="absolute bottom-0 w-[200px]  h-[200px] xl:w-[320px] xl:h-[320px] lg:w-[300px] lg:h-[300px] bg-gradient-to-r from-[#212428] to-[#646942]  flex justify-start items-center"></div>
+            /><div className="absolute -bottom-0 w-[200px]  h-[200px] xl:w-[401px] xl:h-[240px] lg:w-[300px] lg:h-[300px] bg-gradient-to-r from-[#212428] to-designColor  flex justify-start items-center  rounded-lg opacity-65 rounded-tl-[200px] rounded-tr-[80px] "></div>
 
         </div>
     );
