ed4ed68aa784e0c75e3926e0c785d685ae1a1c00
Kabileshwaran183 <Kabileshwaran183@gmail.com>
2025-06-14 14:11:49 +0530

    contact completed

--- src/Components/Contact/Contact.jsx
+++ src/Components/Contact/Contact.jsx
@@ -73,54 +73,54 @@ const Contact = () => {
   return (
     <section
       id="contact"
-      className={`w-full  bg-black border-b-[1px] pad ${theme === 'dark' ? 'border-b-bodyColor-dark/50' : 'border-b-bodyColor-light/50'
+      className={`w-full   border-b-[1px] pad ${theme === 'dark' ? 'border-b-bodyColor-dark/50' : 'border-b-bodyColor-light/50'
         } scroll-mt-[100px]`}
     >
       <div className="flex  justify-center items-center text-center">
         <Title title="CONTACT" des="Contact With Me" />
       </div>
 
-      <div className=" bg-yellow-500 h-auto flex flex-col  lgl:flex-row gap-10 w-[50%] mx-auto">
+      <div className=" h-auto flex flex-col  lgl:flex-row gap-10 w-[50%] mx-auto">
         <div className={`w-full  p-8 ${theme === 'dark'
-            ? 'bg-bodyColor-dark/90 shadow-shadowOne'
-            : 'bg-bodyColor-light shadow-md'
+          ? 'bg-bodyColor-dark/90 shadow-shadowOne'
+          : 'bg-bodyColor-light shadow-md'
           } flex flex-col gap-8  rounded-3xl `}>
 
-            <div className="tab-container">
-  <button
-    className="tab tab--1"
-    onClick={() => {
-      setEmailCard(true);
-      setcontactCard(false);
-    }}
-  >
-    Email
-  </button>
-  <button
-    className="tab tab--2"
-    onClick={() => {
-      setEmailCard(false);
-      setcontactCard(true);
-    }}
-  >
-    Contact Me
-  </button>
-  <div
-    className="indicator"
-    style={{
-      left: emailCard ? "2px" : "calc(50% - 2px)",
-    }}
-  ></div>
-</div>
+          <div className="flex flex-row items-start p-[2px] bg-[#ebebec] rounded-lg mt-2.5 mx-5 relative">
+            <button
+              className="w-4/5 h-10 relative z-[99] bg-transparent border-0 outline-none cursor-pointer font-medium"
+              onClick={() => {
+                setEmailCard(true);
+                setcontactCard(false);
+              }}
+            >
+              Email
+            </button>
+            <button
+              className="w-4/5 h-10 relative z-[99] bg-transparent border-0 outline-none cursor-pointer font-medium"
+              onClick={() => {
+                setEmailCard(false);
+                setcontactCard(true);
+              }}
+            >
+              Contact Me
+            </button>
+            <div
+              className="w-1/2 h-10 bg-yellow-500 absolute top-[2px] z-9 border border-[rgba(0,0,0,0.04)] shadow-[0px_3px_8px_rgba(0,0,0,0.12),_0px_3px_1px_rgba(0,0,0,0.04)] rounded-lg transition-all duration-200 ease-out"
+              style={{
+                left: emailCard ? "2px" : "calc(50% - 2px)",
+              }}
+            ></div>
+          </div>
 
-          
+
 
           {/* Email Card */}
           {emailCard && (
 
             <form
               ref={formRef}
-              className="w-full flex flex-col gap-4 lgl:gap-6 py-2 lgl:py-5"
+              className="w-full flex flex-col gap-4 lgl:gap-6 py-2"
               onSubmit={handleSend}
             >
               {errMsg && (
@@ -133,7 +133,7 @@ const Contact = () => {
                   {successMsg}
                 </p>
               )}
-              <div className="w-full flex flex-col lgl:flex-row gap-10">
+              <div className="w-full flex flex-col lgl:flex-row gap-6">
                 <div className="w-full lgl:w-1/2 flex flex-col gap-4">
                   <p className={`text-sm uppercase tracking-wide ${theme === 'dark' ? 'text-fontColor-dark/80' : 'text-fontColor-light/80'
                     }`}>
@@ -157,8 +157,8 @@ const Contact = () => {
                     onChange={(e) => setPhoneNumber(e.target.value)}
                     value={phoneNumber}
                     className={`${theme === 'dark'
-                        ? 'bg-bodyColor-dark/90 text-fontColor-dark border-2 border-designColor-dark/30 hover:border-designColor-dark/70'
-                        : 'bg-bodyColor-light/90 text-fontColor-light border-2 border-designColor-light/30 hover:border-designColor-light/70'
+                      ? 'bg-bodyColor-dark/90 text-fontColor-dark border-2 border-designColor-dark/30 hover:border-designColor-dark/70'
+                      : 'bg-bodyColor-light/90 text-fontColor-light border-2 border-designColor-light/30 hover:border-designColor-light/70'
                       } contactInput transition-colors duration-300`}
                     type="text"
                   />
@@ -174,8 +174,8 @@ const Contact = () => {
                   onChange={(e) => setEmail(e.target.value)}
                   value={email}
                   className={`${theme === 'dark'
-                      ? 'bg-bodyColor-dark/90 text-fontColor-dark border-2 border-designColor-dark/30 hover:border-designColor-dark/70'
-                      : 'bg-bodyColor-light/90 text-fontColor-light border-2 border-designColor-light/30 hover:border-designColor-light/70'
+                    ? 'bg-bodyColor-dark/90 text-fontColor-dark border-2 border-designColor-dark/30 hover:border-designColor-dark/70'
+                    : 'bg-bodyColor-light/90 text-fontColor-light border-2 border-designColor-light/30 hover:border-designColor-light/70'
                     } contactInput transition-colors duration-300`}
                   type="email"
                 />
@@ -190,8 +190,8 @@ const Contact = () => {
                   onChange={(e) => setSubject(e.target.value)}
                   value={subject}
                   className={`${theme === 'dark'
-                      ? 'bg-bodyColor-dark/90 text-fontColor-dark border-2 border-designColor-dark/30 hover:border-designColor-dark/70'
-                      : 'bg-bodyColor-light/90 text-fontColor-light border-2 border-designColor-light/30 hover:border-designColor-light/70'
+                    ? 'bg-bodyColor-dark/90 text-fontColor-dark border-2 border-designColor-dark/30 hover:border-designColor-dark/70'
+                    : 'bg-bodyColor-light/90 text-fontColor-light border-2 border-designColor-light/30 hover:border-designColor-light/70'
                     } contactInput transition-colors duration-300`}
                   type="text"
                 />
@@ -206,20 +206,20 @@ const Contact = () => {
                   onChange={(e) => setMessage(e.target.value)}
                   value={message}
                   className={`${theme === 'dark'
-                      ? 'bg-bodyColor-dark/90 text-fontColor-dark border-2 border-designColor-dark/30 hover:border-designColor-dark/70'
-                      : 'bg-bodyColor-light/90 text-fontColor-light border-2 border-designColor-light/30 hover:border-designColor-light/70'
-                    } contactTextArea transition-colors duration-300`}
+                    ? 'bg-bodyColor-dark/90 text-fontColor-dark border-2 border-designColor-dark/30 hover:border-designColor-dark/70'
+                    : 'bg-bodyColor-light/90 text-fontColor-light border-2 border-designColor-light/30 hover:border-designColor-light/70'
+                    } contactTextArea h-12 transition-colors duration-300`}
                   cols="30"
                   rows="8"
                 ></textarea>
               </div>
-              <div className="w-full">
+              <div className="w-full text-center">
                 <button
                   type="submit"
-                  className="relative w-full cursor-pointer rounded-[34px] border-2 border-[#c1a362] bg-transparent px-6 py-2.5 text-lg font-semibold text-[#c1a362] overflow-hidden transition-all duration-[600ms] ease-[cubic-bezier(0.23,1,0.32,1)] group"
+                  className="relative w-1/3  cursor-pointer rounded-[34px]  border-2 border-designColor-light/70 bg-designColor-light/50 px-6 py-2.5 text-lg font-semibold text-white overflow-hidden transition-all duration-[600ms] ease-[cubic-bezier(0.23,1,0.32,1)] group"
                 >
                   <span
-                    className="absolute inset-0 m-auto h-12 w-12 scale-0 rounded-full bg-[#c1a362] transition-all duration-[600ms] ease-[cubic-bezier(0.23,1,0.32,1)] -z-10 group-hover:scale-150"
+                    className="absolute inset-0 m-auto h-12 w-12 scale-0 rounded-full bg-designColor-light/70 transition-all duration-[600ms] ease-[cubic-bezier(0.23,1,0.32,1)] -z-10 group-hover:scale-150"
                     style={{ transitionTimingFunction: 'cubic-bezier(0.23, 1, 0.32, 1)' }}
                   ></span>
                   <span className="relative z-10 group-hover:text-[#212121] group-hover:scale-110 group-active:scale-100 transition-all duration-[300ms] ease-[cubic-bezier(0.23,1,0.32,1)]">
@@ -282,8 +282,8 @@ const ContactItem = ({ icon, title, link, label }) => {
     <div className={`flex items-center gap-4 p-4 rounded-lg shadow-md transition-shadow ${theme === 'dark' ? 'bg-bodyColor-dark/80' : 'bg-bodyColor-light/80'
       }`}>
       <span className={`text-2xl ${theme === 'dark'
-          ? 'text-designColor-dark hover:text-designColor-dark/90'
-          : 'text-designColor-light hover:text-designColor-light/90'
+        ? 'text-designColor-dark hover:text-designColor-dark/90'
+        : 'text-designColor-light hover:text-designColor-light/90'
         } transition-colors duration-300`}>{icon}</span>
       <div>
         <h3 className={`text-lg font-medium ${theme === 'dark' ? 'text-fontColor-dark' : 'text-fontColor-light'
